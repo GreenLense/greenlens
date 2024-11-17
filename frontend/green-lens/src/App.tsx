c4a1a492330d14c8eba703c95a70cd9daec304cb
Alan Liang <alanliang905@gmail.com>
2024-11-17 03:37:02 -0600

    animated body section

--- frontend/green-lens/src/App.tsx
+++ frontend/green-lens/src/App.tsx
@@ -8,6 +8,7 @@ import Graphic from './components/ui/graphic'
 import Statement from './components/ui/statement'
 import ImageProvider from './components/ui/image-provider'
 import { useRef } from 'react'
+import {motion } from 'framer-motion'
 
 function App() {
   const targetRef = useRef<HTMLDivElement | null>(null);
@@ -22,10 +23,18 @@ function App() {
   return (
     <div style={{ backgroundColor: '#19B84B', minHeight: '100vh' }}>
       <Hero />
-      <Button onClick={handleClick} className='m-8 bg-white text-green-500 border border-green-500 hover:bg-green-200 focus:outline-none focus:ring-0 hover:border-green-500'>Get Started</Button>
-      <Graphic />
-      <Statement />
-      <ImageProvider ref={targetRef}/>
+      <motion.div
+        initial={{ y: 50, opacity: 0 }}
+        animate={{ y: 0, opacity: 1 }}
+        transition={{ duration: 1.15, delay: 1.75, ease: "easeInOut" }}
+      >
+        <Button onClick={handleClick} className='m-8 bg-white text-green-500 border border-green-500 hover:bg-green-200 focus:outline-none focus:ring-0 hover:border-green-500'>Get Started</Button>
+        <Graphic />
+        <Statement />
+        <ImageProvider ref={targetRef} />
+
+      </motion.div>
+      
     </div>
   )
 }
